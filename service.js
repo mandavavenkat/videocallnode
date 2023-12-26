19c50c8130e40bf667b3f70e0b174aaa3a8c2855
C360 <c360@C360s-MacBook-Pro.local>
2023-12-27 01:20:16 +0530

    pushing code

--- service.js
+++ service.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'http://3.27.202.108:3000/',
     methods: ['GET', 'POST'],
   }
 });
