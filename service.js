7b16fad573e1a8a2965304936c95ae21bfd19fd1
C360 <c360@C360s-MacBook-Pro.local>
2023-12-26 22:43:38 +0530

    first commit

---
+++ service.js
@@ -0,0 +1,36 @@
+
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log(`User connected successfully: ${socket.id}`);
+  socket.emit('me', socket.id)
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('callended')
+  })
+
+  socket.on('calluser',(data)=>{
+    io.to(data.userToCall).emit('calluser',{signal:data.signalData, from:data.from, name: data.name})
+  })
+socket.on('answerCall',(data)=>{
+  io.to(data.to).emit('callAccepted',data.signal)
+})
+
+});
+
+const PORT = 3001;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
